febc2e16586ab066d5d40bb5acabe5332079c6f0
David Hernández Bethencourt <a1ddhernandez@gmail.com>
2013-03-25 20:47:19 +0000

    Usar plantillas para páginas estáticas

--- my_app/views.py
+++ my_app/views.py
@@ -1,15 +1,14 @@
 # Create your views here.
-
-from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 def index(request):
-	return HttpResponse("<h1>Index page</h1>")
+	return render_to_response('index.html')
 
 def home(request):
-	return HttpResponse("<h1>Sample App</h1>")
+	return render_to_response('home.html')
 
 def help(request):
-	return HttpResponse("<h1>Help</h1>")
+	return render_to_response('help.html')
 
 def about(request):
-	return HttpResponse("<h1>About Us</h1>")
+	return render_to_response('about.html')
